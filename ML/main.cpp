14353520ebce8442c4466d18f37d255ac7564893
Umberto Drago <umbertodrago@umbertos-macbook-pro.local>
2016-08-31 22:09:17 +0200

    Initial Commit

---
+++ ML/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ML
+//
+//  Created by Umberto Drago on 31/08/16.
+//  Copyright © 2016 Umberto Drago. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
